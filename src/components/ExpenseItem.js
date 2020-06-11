9e0516ef11d6ad127d3888008a57ff72a39519de
Alican Uver <alicanuver@CAN-MacBook-Air.local>
2020-06-11 20:20:51 +0300

    item and list comp createad

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { List, Button } from 'antd';
+import { EditOutlined, DeleteFilled } from '@ant-design/icons';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ expense }) => {
+
+    console.log(expense)
     return (
-        <div>
-            expense item
-        </div>
+        <>
+            <List.Item
+                actions={[<Button type="primary" icon={<EditOutlined />}>Edit</Button>, <Button type="danger" icon={<DeleteFilled />}>Delete</Button>]}
+            >
+                <List.Item.Meta
+                    title={<h2>{expense.charge}</h2>}
+                    description={expense.description}
+                />
+            </List.Item>
+        </>
+
     )
 }
 
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import Item from './ExpenseItem';
+import React from "react";
+import Item from "./ExpenseItem";
+import { Button, List } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
 
-const ExpenseList = () => {
-    return (
-        <div>
-            expense list
-            <Item />
-        </div>
-    )
-}
+const ExpenseList = ({ expenses }) => {
+  return (
+    <>
+      <List>
+        {expenses.map((expense) => {
+          return <Item key={expense.id} expense={expense} />;
+        })}
+      </List>
+      <div className="text-center">
+        {expenses.length > 0 && (
+          <Button type="danger" shape="round" icon={<DeleteOutlined />}>
+            Clear Expenses
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default ExpenseList
+export default ExpenseList;
