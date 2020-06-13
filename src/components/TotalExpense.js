0a7b3c8f25411432fc9646ba190bf3b223cdb74b
Alican Uver <alicanuver@CAN-MacBook-Air.local>
2020-06-14 00:22:53 +0300

    converted to component

---
+++ src/components/TotalExpense.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import { ExpenseContext } from '../expensecontext/ExpenseContext';
+
+
+const TotalExpense = () => {
+    const [expense, setExpense] = useContext(ExpenseContext);
+    console.log(expense)
+    return (
+            <h1 className="text-center" style={{ marginTop: "30px", textTransform: 'uppercase' }}>
+            total spending:
+            <span style={{ color: 'red' }}>$
+                {
+                    expense.reduce((acc, curr) => {
+                        return acc += curr.amount
+                    }, 0)
+                }
+            </span>
+        </h1>
+    )
+}
+
+export default TotalExpense;
